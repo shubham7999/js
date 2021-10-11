c8272880bf02dfd5db7486c35a4244be773d17bc
Shubham <shubhampokhriyal2018@gmail.com>
2021-10-11 13:43:16 +0530

    bst

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const cube = (x) => {
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  
+    insert(data) {
+      if (data < this.data && this.left) {
+        this.left.insert(data);
+      } else if (data < this.data) {
+        this.left = new Node(data);
+      }
+  
+      if (data > this.data && this.right) {
+        this.right.insert(data);
+      } else if (data > this.data) {
+        this.right = new Node(data);
+      }
+    }
+  
+    contains(data) {
+      if (this.data === data) {
+        return this;
+      }
+  
+      if (data < this.data && this.left) {
+        return this.left.contains(data);
+      } else if (data > this.data && this.right) {
+        return this.right.contains(data);
+      } else {
+        return null;
+      }
+    }
+  }
 
-    console.log(x*x*x);
-}
-
-cube(54445455454545564544554545454545);
-
-
-function square(x)
-{
-    return x*x;
-}
+  
